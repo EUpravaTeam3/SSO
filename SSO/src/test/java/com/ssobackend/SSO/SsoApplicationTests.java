8105498f4ab5e52b114f0f35809f968f459c9d77
Sava Mihajlov <79896945+Savaxc@users.noreply.github.com>
2024-05-16 00:26:57 +0200

    Initial service setup

---
+++ SSO/src/test/java/com/ssobackend/SSO/SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssobackend.SSO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
